a2dba0432f7109816d44f7f860288397289a3841
boufni95 <33513308+boufni95@users.noreply.github.com>
2019-05-09 21:31:10 +0200

    added the listener on the button

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict"
+
+document.getElementById("search-button").addEventListener("click",()=>{
+
+    var bar = document.getElementById("search-bar")
+    console.log("bar")
+    console.log(bar.value)
+})
