cb29e867d74448b86602dc1a54f72208c07f3e8c
boufni95 <33513308+boufni95@users.noreply.github.com>
2019-05-10 13:31:27 +0200

    initial request

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-"use strict"
 
-document.getElementById("search-button").addEventListener("click",()=>{
 
-    var bar = document.getElementById("search-bar")
-    console.log("bar")
-    console.log(bar.value)
-})
+(function () {
+    "use strict"
+    document.getElementById("search-button").addEventListener("click",()=>{
+
+        var bar = document.getElementById("search-bar")
+        console.log("bar")
+        console.log(bar.value)
+    })
+  }());
+(function () {
+    fetch('https://mainnet.blockchainos.org/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
+
+  }());
