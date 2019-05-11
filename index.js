d350a3b8b3c8d49a3ef5c4cc4a5de653e3522a2b
boufni95 <33513308+boufni95@users.noreply.github.com>
2019-05-11 22:23:23 +0200

    make the search for blocks, txs, accounts

--- index.js
+++ index.js
@@ -2,13 +2,31 @@
 
 (function () {
     "use strict"
-    document.getElementById("search-button").addEventListener("click",()=>{
+    document.getElementById("search-block").addEventListener("click",()=>{
 
         var bar = document.getElementById("search-bar")
-        console.log("bar")
+        console.log("block")
         console.log(bar.value)
+        window.location.href = 'https://mainnet.blockchainos.org/api/v1/blocks/'+bar.value;
     })
+    document.getElementById("search-tx").addEventListener("click",()=>{
+
+        var bar = document.getElementById("search-bar")
+        console.log("tx")
+        console.log(bar.value)
+        window.location.href = 'https://mainnet.blockchainos.org/api/v1/transactions/'+bar.value;
+    })
+    document.getElementById("search-account").addEventListener("click",()=>{
+
+        var bar = document.getElementById("search-bar")
+        console.log("account")
+        console.log(bar.value)
+        window.location.href = 'https://mainnet.blockchainos.org/api/v1/accounts/'+bar.value;
+    })
+
   }());
+
+
 (function () {
     fetch('https://mainnet.blockchainos.org/')
   .then(function(response) {
